127ff74db83ee5f5c3eca69fd1445f34cdd1cbe8
Aji Muhammad Zapar <ajizapar@AJIZAPAR-ID-MAC-LX4W9RF7PP.local>
2025-07-06 23:36:40 +0700

    FEAT: delete data from client

--- backend/db/supabase_client.py
+++ backend/db/supabase_client.py
@@ -3,8 +3,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SUPABASE_URL = os.getenv("SUPABASE_URL", "https://yyrqotproacvfauudnak.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5cnFvdHByb2FjdmZhdXVkbmFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MTQ5ODgsImV4cCI6MjA2NzI5MDk4OH0.i3U7ZPW7LuUCKcPqy82wVn4cqE9Cbrlmx98M-4expus")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 print(SUPABASE_URL, SUPABASE_KEY)
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
