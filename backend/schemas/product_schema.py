953bcdf47f73c1a6a8e26c1608a981d209e75da4
Aji Muhammad Zapar <ajizapar@AJIZAPAR-ID-MAC-LX4W9RF7PP.local>
2025-07-07 21:52:16 +0700

    FEAT: change product data table

--- backend/schemas/product_schema.py
+++ backend/schemas/product_schema.py
@@ -8,9 +8,15 @@ class Review(BaseModel):
 
 class ProductMetadata(BaseModel):
     id: str
-    title: str
-    material_info: str
+    product_id: str  # UUID tambahan
+    name: str        # ganti dari title ke name
+    material_info: Optional[str] = None  # nullable
     description: str
+    brand: str
+    gender: str
+    category: str
+    product_link: str
+    image: str
     reviews: Optional[List[Review]] = []
 
 # Standard response wrapper
