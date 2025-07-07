f9aaa69873dbfc019a63c5771a2da5f10b7f10a7
Aji Muhammad Zapar <55468476+Amzar123@users.noreply.github.com>
2025-07-07 21:58:29 +0700

    FEAT: fix undefined variable (#33)
    Co-authored-by: Aji Muhammad Zapar <ajizapar@AJIZAPAR-ID-MAC-LX4W9RF7PP.local>
    Co-authored-by: Shuhao Zhang <shuhao.qgg.zhang@gmail.com>

--- backend/api/handlers/ai_handler.py
+++ backend/api/handlers/ai_handler.py
@@ -57,8 +57,7 @@ def object_detector(payload: ImagePayload):
     image = Image.open(BytesIO(image_data)).convert("RGB")
     # Object Detection
     with torch.no_grad():
-        inputs = Initializer.yolo_image_processor(
-            images=[image], return_tensors="pt")
+        inputs = Initializer.yolo_image_processor(images=[image], return_tensors="pt")
         outputs = Initializer.yolo_model(**inputs.to(Initializer.device))
         target_sizes = torch.tensor([[image.size[1], image.size[0]]])
         results = Initializer.yolo_image_processor.post_process_object_detection(
@@ -71,8 +70,7 @@ def object_detector(payload: ImagePayload):
             label = label.item()
             box = [i.item() for i in box]
             items["scores"].append(score)
-            items["labels"].append(
-                Initializer.yolo_model.config.id2label[label])
+            items["labels"].append(Initializer.yolo_model.config.id2label[label])
             items["bboxes"].append(box)
     # Return Output
     return items
@@ -114,8 +112,7 @@ def image_retrieval(payload: DetectionInput, k: int = Query(...)):
         images=cropped_objects, return_tensors="pt")
     for i in range(inputs['pixel_values'].size(0)):
         feature = inputs['pixel_values'][i].unsqueeze(0)
-        image_features = Initializer.clip_model.get_image_features(
-            feature.to(Initializer.device))
+        image_features = Initializer.clip_model.get_image_features(feature.to(Initializer.device))
         # Normalize the features
         image_features = image_features / \
             image_features.norm(p=2, dim=-1, keepdim=True)
